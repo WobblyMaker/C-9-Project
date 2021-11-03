44a4ca4ff098b863e1ed60f05b400025d892d291
WobblyMaker <93649968+WobblyMaker@users.noreply.github.com>
2021-11-03 16:21:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(0)
+
+}
+
+function draw() {
+  
+  if(keyIsDown(RIGHT_ARROW)){
+    background('red');
+  }
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) {
+    background('blue');
+    
+  }
+ 
+  if (keyIsDown(UP_ARROW)) {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
+
